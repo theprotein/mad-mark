7bf6da10161be46e6e5e2c0bc242c345df7fa430
Vladimir Grinenko <i@tadatuta.com>
2014-06-17 03:45:00 +0400

    fix ignore

---
+++ desktop.bundles/index/index.bemdecl.js
@@ -0,0 +1,19 @@
+exports.blocks = [
+    {
+        "name": "root"
+    },
+    {
+        "name": "page",
+        "elems": [
+            {
+                "name": "meta"
+            },
+            {
+                "name": "css"
+            },
+            {
+                "name": "js"
+            }
+        ]
+    }
+];
