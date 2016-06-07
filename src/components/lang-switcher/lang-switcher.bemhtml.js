5bebdd8467a7ef752e231881dc01609df52553dc
Aleksey Guryanov <gurianov@gmail.com>
2016-06-07 17:52:09 +0400

    lang-switcher: generate actual links (#42)
    closes #34

--- src/components/lang-switcher/lang-switcher.bemhtml.js
+++ src/components/lang-switcher/lang-switcher.bemhtml.js
@@ -1,9 +1,13 @@
 block('lang-switcher')(
   content()(function () {
+    const langs = this._config.langs;
+    const mainLang = langs[0];
+    const baseUrl = this._layout === 'root' ? '' : this._layout;  
+
     return this._config.langs.map(lang => ([
       {
         block: 'link',
-        url: '#',
+        url: `/${baseUrl}/${this._name}${lang === mainLang ? '' : '.' + lang}.html`,
         content: lang
       },
       '&nbsp;'
