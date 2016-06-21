9a5f7f7a1a648b7a9c0ed1f3a3277639e8b62628
Ivan Voischev <voischev@yandex-team.ru>
2016-06-22 02:50:53 +0300

    refactor(posthtml): match chaining

--- src/lib/posthtml-plugins/semantize.js
+++ src/lib/posthtml-plugins/semantize.js
@@ -2,68 +2,58 @@
 
 module.exports = function semantize(tree) {
 
-  tree.match({ tag: 'a', attrs: { class: false } }, node => {
-    node.block = 'link';
-    return node;
-  });
-
-  tree.match({ tag: 'ul' }, node => {
-    node.block = 'list';
-    return node;
-  });
-
-  tree.match({ tag: 'ol' }, node => {
-    node.block = 'list';
-    node.mods = { type: 'ordered' };
-    return node;
-  });
-
-  tree.match({ tag: 'li' }, node => {
-    node.block = 'list';
-    node.elem = 'item';
-    return node;
-  });
-
-  tree.match({ tag: 'p' }, node => {
-    node.block = 'paragraph';
-    return node;
-  });
-
-  tree.match({ tag: /^h\d$/ }, node => {
-    node.block = 'heading';
-    node.mods = { lvl: node.tag.charAt(1) };
-    return node;
-  });
-
-  tree.match({ tag: 'table' }, node => {
-    node.block = 'table';
-    return node;
-  });
-
-  tree.match({ tag: 'img' }, node => {
-    node.block = 'image';
-    return node;
-  });
-
-  tree.match({ tag: 'code' }, node => {
-    node.block = 'code';
-    return node;
-  });
-
-  tree.match({ tag: 'blockquote' }, node => {
-    node.block = 'blockquote';
-    return node;
-  });
-
-  tree.match({ tag: 'input', attrs: { type: 'checkbox' } }, node => {
-    node.block = 'checkbox';
-    return node;
-  });
-
-  tree.match({ attrs: { disabled: true } }, node => {
-    node.mods = { disabled: true };
-    return node;
-  });
+  tree
+    .match({ tag: 'a', attrs: { class: false } }, node => {
+      node.block = 'link';
+      return node;
+    })
+    .match({ tag: 'ul' }, node => {
+      node.block = 'list';
+      return node;
+    })
+    .match({ tag: 'ol' }, node => {
+      node.block = 'list';
+      node.mods = { type: 'ordered' };
+      return node;
+    })
+    .match({ tag: 'li' }, node => {
+      node.block = 'list';
+      node.elem = 'item';
+      return node;
+    })
+    .match({ tag: 'p' }, node => {
+      node.block = 'paragraph';
+      return node;
+    })
+    .match({ tag: /^h\d$/ }, node => {
+      node.block = 'heading';
+      node.mods = { lvl: node.tag.charAt(1) };
+      return node;
+    })
+    .match({ tag: 'table' }, node => {
+      node.block = 'table';
+      return node;
+    })
+    .match({ tag: 'img' }, node => {
+      node.block = 'image';
+      return node;
+    })
+    .match({ tag: 'code' }, node => {
+      node.block = 'code';
+      return node;
+    })
+    .match({ tag: 'blockquote' }, node => {
+      node.block = 'blockquote';
+      return node;
+    })
+    .match({ tag: 'input', attrs: { type: 'checkbox' } }, node => {
+      node.block = 'checkbox';
+      return node;
+    })
+    .match({ attrs: { disabled: true } }, node => {
+      node.mods = { disabled: true };
+      return node;
+    });
 
   return tree;
 };
